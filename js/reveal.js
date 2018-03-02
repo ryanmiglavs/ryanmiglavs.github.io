f0837221237732cea426f09ca293920c9bbfc9ec
Ryan Miglavs <ryan.miglavs@ombuweb.com>
2018-03-01 18:12:13 -0800

    Improves reveal/hide for samples and adds scrolling as needed.

--- js/reveal.js
+++ js/reveal.js
@@ -6,6 +6,10 @@
 */
 
 (function () {
+	var prefersReducedMotion = matchMedia('(prefers-reduced-motion)');
+	var isSmoothScrollSupported = 'scrollBehavior' in document.documentElement.style;
+	
+	
 	// Initialize when the DOM is ready
 	// 	• Find and store DOM elements
 	//	• Set up click handler for see samples elements
@@ -19,7 +23,22 @@
 			
 			viewSamplesLink.addEventListener('click', function(e) {
 				if ( li.classList.contains('expanded') ) {
+					var offsetTop = getPageOffset(li.querySelector('.project-intro'));
+					
 					li.classList.remove('expanded');
+					
+					if ( window.pageYOffset > (offsetTop + window.innerHeight) ) {
+						if (!prefersReducedMotion && isSmoothScrollSupported) {
+							window.scrollTo({
+								'behavior': 'smooth',
+								'left': 0,
+								'top': offsetTop,
+							});
+						} else {
+							window.scrollTo(0, offsetTop);
+						}
+					}
+					
 				} else {
 					li.classList.add('expanded');
 				}
@@ -28,4 +47,16 @@
 			});
 		});
 	});
+	
+	function getPageOffset(el) {
+		var currentOffset = 0;
+		
+		if (el.offsetParent) {
+			do {
+				currentOffset += el.offsetTop;
+			} while (el = el.offsetParent);
+		}
+		
+		return currentOffset;
+	}
 })();
